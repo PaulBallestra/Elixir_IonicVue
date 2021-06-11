8d0d40b331e0227507cbc4ecb8985ca1283405ef
PaulBallestra <paulballestra@yahoo.fr>
2021-06-11 16:38:08 +0200

    Login content page added

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,19 +1,55 @@
 <template>
 
-    <ion-page>
+    <ion-page style="background-color: rgba(255, 255, 255, 1);">
 
-        Login
+        <NavBar></NavBar>
+
+        <ion-content>
+
+            <!-- HEADER PAGE -->
+            <ion-grid>
+                <ion-row>
+                    <ion-col>
+                        <ion-grid style="margin-top: 22.5vh;">
+                            <ion-row>
+                                <h3 class="titlePage">LOGIN</h3>
+                            </ion-row>
+                        </ion-grid>
+                        <div class="loginDiv" style="margin: auto; padding-bottom: 15px;">
+                            <ion-item>
+                                <ion-label position="stacked" style="--ion-font-family: 'Montserrat';">Email</ion-label>
+                                <ion-input class="inputCustom"></ion-input>
+                            </ion-item>
+
+                            <ion-item>
+                                <ion-label position="stacked" style="--ion-font-family: 'Montserrat';">Mot de passe</ion-label>
+                                <ion-input class="inputCustom"></ion-input>
+                            </ion-item>
+                        </div>
+                    </ion-col>
+                </ion-row>
+            </ion-grid>
+
+        </ion-content>
 
     </ion-page>
 </template>
 
 <script>
 
-    import { IonPage } from "@ionic/vue";
+    import NavBar from '../components/NavBar.vue'
+    import { IonPage, IonGrid, IonRow, IonContent, IonLabel, IonInput, IonItem } from "@ionic/vue";
 
     export default {
         components: {
             IonPage,
+            IonGrid,
+            IonRow,
+            IonContent,
+            NavBar,
+            IonLabel,
+            IonInput,
+            IonItem
         }
 
     }
