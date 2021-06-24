52de0b73c560cd6781d2647c7fea4e089de3ba85
PaulBallestra <paulballestra@yahoo.fr>
2021-06-24 18:51:02 +0200

    Login is now connected to API

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,5 +1,37 @@
 import axios from 'axios'
 
+
+//LOGIN
+export const login = ({ commit }, form) => {
+
+    axios.post(
+        'http://127.0.0.1:8000/api/auth/login',
+        {
+            email: form.email,
+            password: form.password,
+        }
+    ).then((response) => {
+
+        console.log(response);
+
+        commit('token', response.data.token);
+
+        const user = {
+            family_name: response.data.family_name,
+            given_name: response.data.given_name,
+            email: response.data.email,
+            created_at: response.data.created_at
+        };
+
+        commit('data', user)
+
+    }).catch((error) => {
+        console.log(error)
+    });
+
+};
+
+//REGISTER
 export const register = ({ commit }, form) => {
 
     axios.post(
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -15,20 +15,21 @@
                                 <h3 class="titlePage" style="color: black;">LOGIN</h3>
                             </ion-row>
                         </ion-grid>
-                        <div class="loginDiv">
+                        <form @submit.prevent="login(form)" class="loginDiv">
                             <ion-item>
                                 <ion-label position="stacked" style="--ion-font-family: 'Montserrat';">Email</ion-label>
-                                <ion-input class="inputCustom" type="email"></ion-input>
+                                <ion-input class="inputCustom" type="email" id="email" v-model="form.email"></ion-input>
                             </ion-item>
 
                             <ion-item>
                                 <ion-label position="stacked" style="--ion-font-family: 'Montserrat';">Mot de passe</ion-label>
-                                <ion-input type="password" class="inputCustom"></ion-input>
+                                <ion-input type="password" class="inputCustom" id="password" v-model="form.password"></ion-input>
                             </ion-item>
 
-                        </div>
+                            <ion-button class="btnCustom" style="margin: 5px auto;" expand="full" type="submit" color="primary">Login</ion-button>
 
-                        <ion-button class="btnCustom" style="margin: 5px auto;" expand="full" color="primary">Login</ion-button>
+                        </form>
+
 
                         <p class="subButtonLabel" style="margin-bottom: 33vh;"> Mot de passe oublié ? </p>
 
@@ -50,7 +51,15 @@
     import Footer from '../components/Footer.vue'
     import { IonPage, IonGrid, IonRow, IonContent, IonCol, IonLabel, IonInput, IonItem, IonButton } from "@ionic/vue";
 
+    import { mapActions } from 'vuex';
+
     export default {
+        name: 'Login',
+        data() {
+            return {
+                form: {}
+            };
+        },
         components: {
             IonPage,
             IonGrid,
@@ -63,6 +72,9 @@
             IonCol,
             Footer,
             IonButton
+        },
+        methods: {
+            ...mapActions({'login': 'auth/login'})
         }
 
     }
